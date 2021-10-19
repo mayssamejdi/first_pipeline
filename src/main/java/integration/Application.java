64cde6075124b4a4719ad7796710edbdbcb6526e
mayssa <mayssa.mejdi@inetum.com>
2021-10-19 11:04:07 +0200

    docker file

--- src/main/java/integration/Application.java
+++ src/main/java/integration/Application.java
@@ -1,5 +1,6 @@
 package integration;
 
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,6 +16,10 @@ public class Application {
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+	@Bean
+	public KeycloakSpringBootConfigResolver keycloakConfigResolver() {
+		return new KeycloakSpringBootConfigResolver();
 	}
 
 }
